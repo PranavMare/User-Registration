11e7611599c058f7a45a370bc9a2d4ed22569d20
Pranav <pranavmare99@gmail.com>
2020-09-05 00:00:44 +0530

    Rule 2

---
+++ validPasswordR2.sh
@@ -0,0 +1,67 @@
+
+#! /bin/bash -x
+
+echo "Enter your first name : " 
+read firstname
+
+firstname_regex=^[[:upper:]][a-z]{2,}$
+
+if [[ $firstname =~ $firstname_regex ]]
+then
+    echo "First name is valid "
+else
+    echo "First name is invalid"
+fi
+
+echo "Enter your last name : " 
+read lastname
+
+lastname_regex=^[[:upper:]][a-z]{2,}$
+
+if [[ $lastname =~ $lastname_regex ]]
+then
+    echo "Last Name is valid"
+else
+    echo "Last Name is Invalid"
+fi
+
+read -p "Enter your Email-Id : " email
+
+email_regex=^abc[.][a-z]{3}@bl[.]co[.][a-z]{2}$
+
+if [[ $email =~ $email_regex ]]
+then
+    echo "Email-Id is valid"
+else
+    echo "Emai-Id is invalid"
+fi
+
+read -p "Enter your mobile no: " number
+
+number_regex=^[0-9]{2}[" "][0-9]{10}$
+
+if [[ $number =~ $number_regex ]]
+then
+    echo "mobile number is valid"
+else    
+    echo "mobile numbe is invalid"
+fi
+
+read -p "Enter the password : " pass1
+
+pass1_regex=^[a-zA-Z0-9@/.,_~%^&\*]{8,}$
+
+if [[ $pass1 =~ $pass1_regex ]]
+then
+    echo "Password rule1 is valid"
+else
+    echo "password rule1 is invalid"
+fi
+
+
+if [[ ${#pass1} -ge 8 && "$pass1" == *[A-Z]* && "$pass1" == *[a-z]* ]]
+then
+        echo "password rule2 is valid "
+else
+        echo "password must contain atleast a number, capital letter and 8 characters"
+fi
